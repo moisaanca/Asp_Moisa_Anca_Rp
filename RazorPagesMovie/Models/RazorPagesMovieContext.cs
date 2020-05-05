623e586489f20c093f4c3771f74e6f09cca1f808
anca.moisa <anca.moisa@cgm.com>
2020-05-05 15:23:23 +0300

    Added razor page project

---
+++ RazorPagesMovie/Models/RazorPagesMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Models
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+        public RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; }
+    }
+}
